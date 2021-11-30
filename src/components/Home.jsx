36e9fb89a406c53b5ba3abc29c27e373bff90284
francorosa <km115.franco@gmail.com>
2021-11-29 20:30:48 -0500

    Add n boxes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,14 +4,14 @@ import Box from "./Box";
 
 const Home = () => {
   const boxes = useStoreState((state) => state.boxes);
-  const elements = new Array(parseInt(boxes));
+  const elements = [...Array(parseInt(boxes)).keys()];
   console.log(elements);
   return (
     <div>
       <h1>This is Home</h1>
       <p>This is another brick in the wall</p>
       {elements.map((x) => (
-        <Box />
+        <Box key={x} />
       ))}
       <Navigator to="/config" />
     </div>
